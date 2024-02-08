5052b869d05d4865d4d4721b236d624ca96280ca
PierreHerrera <pierreherreraoropeza@gmail.com>
2024-02-08 16:49:06 -0500

    Listasr posts y btn para editar

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('admin.posts.index');
+        $posts = Post::latest('id')->paginate(10);
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
