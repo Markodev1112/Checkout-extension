3ccd5df36e42390291158edc4d29260844640d91
PierreHerrera <pierreherreraoropeza@gmail.com>
2024-02-07 22:51:46 -0500

    Factories y ejecución del seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -16,8 +16,21 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $published = $this->faker->randomElement([true,false]);
+        // $published_at = $published ? $this->faker->dateTimeBetween('-1 month') : null;
+        $published_at = $published ? now() : null;
+
         return [
             //
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'excerpt' => $this->faker->text(200),
+            'body' => $this->faker->text(2000),
+            'image_path' => $this->faker->imageUrl(1280, 720),
+            'published' => $published,
+            'category_id' => rand(1, 5),
+            'user_id' => rand(1, 20),
+            'published_at' => $published_at,
         ];
     }
 }
