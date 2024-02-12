393666849b7c42c0a1aa7e57e6042977ead029a0
PierreHerrera <pierreherreraoropeza@gmail.com>
2024-02-12 10:58:04 -0500

    Replace JS para el slug

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,7 +4,10 @@
         Nuevo artículo
     </h1>
 
-    <form action="{{route('admin.posts.store')}}" method="POST">
+    <form action="{{route('admin.posts.store')}}"
+        method="POST"
+        x-data="data()"
+        x-init="$watch( 'title', value => { string_to_slug(value)} )">
     
         @csrf
 
@@ -17,6 +20,7 @@
 
             <x-input
                 name="title"
+                x-model="title"
                 value="{{ old('title') }}"
                 class="w-full"
                 placeholder="Ingrese el nombre del artículo"
@@ -30,6 +34,7 @@
 
             <x-input
                 name="slug"
+                x-model="slug"
                 value="{{ old('slug') }}"
                 class="w-full"
                 placeholder="Ingrese el slug del artículo"
@@ -56,5 +61,29 @@
         </div>
 
     </form>
+
+    @push('js')
+        <script>
+            function data(){
+                return {
+                    title: '',
+                    slug: '',
+                    string_to_slug(str){
+                        str = str.replace(/^\s+|\s+$/g, '');
+                        str = str.toLowerCase();
+                        var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+                        var to = "aaaaeeeeiiiioooouuuunc------";
+                        for (var i = 0, l = from.length; i < l; i++) {
+                            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+                        }
+                        str = str.replace(/[^a-z0-9 -]/g, '')
+                            .replace(/\s+/g, '-')
+                            .replace(/-+/g, '-');
+                        this.slug = str;
+                    }
+                }
+            }
+        </script>
+    @endpush
     
 </x-admin-layout>
