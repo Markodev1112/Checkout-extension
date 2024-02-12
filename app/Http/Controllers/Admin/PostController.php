1c7c5f2a626cad75eab836fa143d0df4e14c4183
PierreHerrera <pierreherreraoropeza@gmail.com>
2024-02-12 11:44:08 -0500

    Observer para precargar el user_id al guardar un registro

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -39,12 +39,7 @@ class PostController extends Controller
             'category_id' => 'required|exists:categories,id',
         ]);
 
-        $post = Post::create([
-            'title' => $request->title,
-            'slug' => $request->slug,
-            'category_id' => $request->category_id,
-            'user_id' => auth()->id(),
-        ]);
+        $post = Post::create($request->all());
 
         session()->flash('swal',[
             'icon' => 'success',
